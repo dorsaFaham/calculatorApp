32507ef47758d3a20c97ed78178c1345457642b0
dorsa <dorsa.faham.nanadi.df@gmail.com>
2025-03-06 20:55:22 +0100

    created base Tk file

---
+++ tkCalc.py
@@ -0,0 +1,15 @@
+#tk calculator
+
+from tkinter import *
+
+root = Tk()
+root.geometry("350x385")
+root.resizable(0,0)
+root.configure(bg = "lightblue")
+root.title("calculator")
+
+
+
+
+
+root.mainloop()
